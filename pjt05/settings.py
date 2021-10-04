c70551bc626d1551672c65ea54f38bf38e1633a5
jae961217 <jae961217@gmail.com>
2021-10-05 00:13:25 +0900

    Apply .gitignore

--- pjt05/settings.py
+++ pjt05/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os, json
 from pathlib import Path
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u2(d73jv6%^a26bi#q$w+wq_97fmh*mifve2qza-&l6j#7tnc+'
+secret_file = os.path.join(BASE_DIR,'secrets.json')
+
+with open(secret_file,'r') as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting,secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg=f"Set the {setting} environment variable"
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
